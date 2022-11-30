65d3274f08c52358c8e09687f03fb4da3cbda554
Syed Ziaul Habib <roobon@gmail.com>
2022-11-30 12:59:12 +0600

    Home page has come.

--- src/pages/FrontPage.js
+++ src/pages/FrontPage.js
@@ -210,7 +210,169 @@ export default function FrontPage() {
           </a>
         </div>
       </section>
+      <div className="three_box">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <div className="box_text">
+                <h3>AUTO DIAGNOSE</h3>
+                <i>
+                  <img src="assets/images/thr.png" alt="#" />
+                </i>
+                <p>
+                  ipsum dolor sit amet, consectetur adipiscing elit, sed d
+                  veniam, adipiscing elit, sed d veniam
+                </p>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="box_text">
+                <h3>AUTO DIAGNOSE</h3>
+                <i>
+                  <img src="assets/images/thr1.png" alt="#" />
+                </i>
+                <p>
+                  ipsum dolor sit amet, consectetur adipiscing elit, sed d
+                  veniam, adipiscing elit, sed d veniam
+                </p>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="box_text">
+                <h3>AUTO DIAGNOSE</h3>
+                <i>
+                  <img src="assets/images/thr2.png" alt="#" />
+                </i>
+                <p>
+                  ipsum dolor sit amet, consectetur adipiscing elit, sed d
+                  veniam, adipiscing elit, sed d veniam
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* About */}
+      <div className="about">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="titlepage">
+                <h2>About Our Car Service </h2>
+              </div>
+            </div>
+          </div>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-10 offset-md-1">
+                <div className="about_img">
+                  <div className="about_box">
+                    <h3>Dolor sit amet, consectetur adipiscing elit, </h3>
+                    <p>
+                      dolor sit amet, consectetur adipiscing elit, sed do
+                      eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud dolor sit
+                      amet, consectetur adipiscing elit, sed do eiusmod tempor
+                      incididunt ut labore et dolore magna aliqua. Ut enim ad
+                      minim veniam, quis nostrud dolor sit amet, consectetur
+                      adipiscing elit, sed do eiusmod tempor incididunt ut
+                      labore et dolore magna aliqua. Ut enim ad minim veniam,
+                      quis nostrud dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud{" "}
+                    </p>
+                    <a className="read_more">Read More</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <!-- end about -->
+      <!-- wedo  section --> */}
+      <div className="wedo ">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="titlepage">
+                <h2>What We Do</h2>
+                <p>
+                  It is a long established fact that a reader will be distracted
+                  by the{" "}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-10 offset-md-1">
+              <div className="row">
+                <div className="col-md-6 margin_bottom">
+                  <div className="work">
+                    <figure>
+                      <img src="assets/images/img1.png" alt="#" />
+                    </figure>
+                  </div>
+                  <div className="work_text">
+                    <h3>
+                      Quick repair
+                      <br />
+                      <span className="blu">Total Service</span>
+                    </h3>
+                  </div>
+                </div>
+                <div className="col-md-6 margin_bottom">
+                  <div className="work">
+                    <figure>
+                      <img src="assets/images/img2.png" alt="#" />
+                    </figure>
+                  </div>
+                  <div className="work_text">
+                    <h3>
+                      Quick repair
+                      <br />
+                      <span className="blu">Total Service</span>
+                    </h3>
+                  </div>
+                </div>
+                <div className="col-md-6 margin_bottom">
+                  <div className="work">
+                    <figure>
+                      <img src="assets/images/img3.png" alt="#" />
+                    </figure>
+                  </div>
+                  <div className="work_text">
+                    <h3>
+                      Quick repair
+                      <br /> <span className="blu">Total Service</span>
+                    </h3>
+                  </div>
+                </div>
+                <div className="col-md-6 margin_bottom">
+                  <div className="work">
+                    <figure>
+                      <img src="assets/images/img4.png" alt="#" />
+                    </figure>
+                  </div>
+                  <div className="work_text">
+                    <h3>
+                      Quick repair
+                      <br />
+                      <span className="blu">Total Service</span>
+                    </h3>
+                  </div>
+                </div>
+              </div>
+              <a className="read_more" href="#">
+                See More
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
       ;
+      {/* <!-- end wedo  section -->
+      <!-- testimonial --> */}
     </>
   );
 }
