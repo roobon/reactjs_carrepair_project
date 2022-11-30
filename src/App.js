61982dd0e822de95e85f5c7721b701c9a7678f28
Syed Ziaul Habib <roobon@gmail.com>
2022-11-30 12:44:06 +0600

    Integrated template files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import FrontPage from "./pages/FrontPage";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<FrontPage />}>
+        {/* <Route index element={<Home />} /> */}
+        {/* <Route path="/about" element={<About />} /> */}
+      </Route>
+    </Routes>
   );
 }
 
